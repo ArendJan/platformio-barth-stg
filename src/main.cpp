9d940f1de6402b74df34a2366de7f99eb8a49f47
Arend-Jan van Hilten <arendjan18@gmail.com>
2024-08-04 11:37:40 +0200

    format according to .clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,30 @@
 #include <hal/can_hal.hpp>
 #include <hal/io_hal.hpp>
 
-auto const LED_RED = 1; // PC_15
+auto const LED_RED = 1;  // PC_15
 
-void cb(uint32_t id, uint8_t const *data, size_t size) {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  // fdcan1.send(id, data, size); // NOTE: this will trigger the InfiniteLoop,
-  // as the callback is ran in the interrupt context
+void cb(uint32_t id, uint8_t const* data, size_t size)
+{
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    // fdcan1.send(id, data, size); // NOTE: this will trigger the InfiniteLoop,
+    // as the callback is ran in the interrupt context
 }
 
-void setup() {
-  io_init();
+void setup()
+{
+    io_init();
 
-  fdcan1.on_receive(&cb);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
+    fdcan1.on_receive(&cb);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
 }
 
-void loop() {
-  delay(100);
-  static uint8_t i = 0;
-  uint8_t data[] = {1, 2, 3, i++};
-  fdcan1.send(101, data, 4);
-  // can::fdcan2.send(102, data, 4);
-  digitalWrite(LED_RED, i % 2);
+void loop()
+{
+    delay(100);
+    static uint8_t i = 0;
+    uint8_t data[] = {1, 2, 3, i++};
+    fdcan1.send(101, data, 4);
+    // can::fdcan2.send(102, data, 4);
+    digitalWrite(LED_RED, i % 2);
 }
